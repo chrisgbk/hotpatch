cab4e46cc6faa94cc2d40cf7c0bb5e0e29ec9769
Chris Greenbank <chrisgbk@hotmail.com>
2018-11-17 03:38:07 -0700

    1.0.1

--- scenarios/Hotpatch/control.lua
+++ scenarios/Hotpatch/control.lua
@@ -17,6 +17,10 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 -- probably some performance improvements to be made by eliminating usage of the hash part of tables and sticking to the array part
 -- events, loaded mods, etc etc
 
+local debug_mode = false
+local compat_mode = false
+local sctrict_mode = false
+
 -- these represent the mods as packaged with the scenario
 local initial_name = {}
 local initial_version = {}
@@ -45,6 +49,11 @@ new_mod('my-mod', '1.0.0', [===[
 
 -- This is where the fun begins
 
+local function debug_log(what)
+    if debug_mode then 
+        log(what)
+    end
+end
 -- loaded mods, these are always dynamically loaded, not saved to global
 -- this holds a reference to the mods script object
 local loaded_mods = {}
@@ -67,18 +76,23 @@ end
 --TODO: pretty much all of this routine, which is unused currently
 -- This should unregister events and clear the globals
 local function mod_reset(mod_name)
-    --for k, v in pairs(global.globals[mod_name]) do
-    --    global.globals[mod_name][k] = nil
-    --end
-    --mod_reset_events(mod_name)
+    local mod_global = global.globals[mod_name]
+    for k, v in pairs(mod_global) do
+        mod_global[k] = nil
+    end
+    mod_reset_events(mod_name)
 end
 
 local function mod_reset_events(mod_name)
-    --for k, v in pairs(defines.events) do
-    --    if (k ~= 'on_load') and (k ~= 'on_init') and (k ~= 'on_configuration_changed') then
-    --        script.on_event(v, nil)
-    --    end
-    --end
+    --TODO: deregister global event handlers that aren't needed anymore
+    local mod_script = loaded_mods[mod_name]
+    if mod_script then 
+        mod_script.__events = {}
+        mod_script.__ticks = {}
+        mod_script.__on_init = nil
+        mod_script.__on_load = nil
+        mod_script.__on_configuration_changed = nil
+    end
 end
 
 -- TODO: handle mod load failure cases
@@ -107,11 +121,26 @@ local function run_mod(mod_name)
         mod_script.on_configuration_changed = function(f)
             mod_script.__on_configuration_changed = f
         end
-        mod_script.on_event = function(event, f)
-            mod_script.__events[event] = f
-         end
-        mod_script.on_nth_tick = function(tick, f)
-            mod_script.__ticks[tick] = f
+        if not compat_mode then
+            mod_script.on_event = function(event, f)
+                mod_script.__events[event] = f
+            end
+            mod_script.on_nth_tick = function(tick, f)
+                mod_script.__ticks[tick] = f
+            end
+        else   
+            mod_script.on_event = function(event, f)
+                mod_script.__events[event] = f
+                if mod_script.__loaded then
+                    script.raise_event(mod_script.__notify, {mod=mod_script.__mod_name, event=event})
+                end
+            end
+            mod_script.on_nth_tick = function(tick, f)
+                mod_script.__ticks[tick] = f
+                if mod_script.__loaded then
+                    script.raise_event(mod_script.__notify, {mod=script.__mod_name, event='on_nth_tick', nth_tick=tick})
+                end
+            end
         end
         mod_script.generate_event_name = function()
             return script.generate_event_name()
@@ -147,17 +176,32 @@ local function run_mod(mod_name)
         --load/run code
         local mod_code = load(global.mod_code[mod_name], 'hotpatch loader', 'bt', environment)
         mod_env[mod_name] = environment
+        
+        if strict_mode then
+           environment.global = {}
+        end
         mod_code()
+        if compat_mode then
+            mod_script.__loaded = true
+        end
+        if strict_mode then
+            if environment.global ~= {} then
+                --TODO: error, mod touched global inappropriately during load
+            end
+           environment.global = global.globals[mod_name]
+        end
         loaded_mods[mod_name] = mod_script
         
         --load complete, start notifying on event subscriptions
-        mod_script.on_event = function(event, f)
-            mod_script.__events[event] = f
-            script.raise_event(mod_script.__notify, {mod=mod_script.__mod_name, event=event})
-        end
-        mod_script.on_nth_tick = function(tick, f)
-            mod_script.__ticks[tick] = f
-            script.raise_event(mod_script.__notify, {mod=script.__mod_name, event='on_nth_tick', nth_tick=tick})
+        if not compat_mode then
+            mod_script.on_event = function(event, f)
+                mod_script.__events[event] = f
+                script.raise_event(mod_script.__notify, {mod=mod_script.__mod_name, event=event})
+            end
+            mod_script.on_nth_tick = function(tick, f)
+                mod_script.__ticks[tick] = f
+                script.raise_event(mod_script.__notify, {mod=script.__mod_name, event='on_nth_tick', nth_tick=tick})
+            end
         end
     end
 end
@@ -269,9 +313,12 @@ remote_interface['install'] = function(mod_name, mod_version, mod_code)
     -- Note that mods may expect that certain events haven't been called yet when their on_init is ran
     -- This may prevent them from functioning properly, without manually calling the events they expect
     -- examples: on_player_created
-    install_mod(mod_name, mod_version, mod_code)
-    run_mod(mod_name)
-    mod_init(mod_name)
+    local caller = game.player or _ENV
+    if (caller == _ENV) or caller.admin then
+        install_mod(mod_name, mod_version, mod_code)
+        run_mod(mod_name)
+        mod_init(mod_name)
+    end
 end
 
 remote_interface['update'] = function(mod_name, mod_version, mod_code)
@@ -279,15 +326,18 @@ remote_interface['update'] = function(mod_name, mod_version, mod_code)
     -- the current mods events are de-registered, the new code is installed, on_load is triggered, and then events are registered
     -- finally, the mod is informed of the update, so it can run migrations from the previous version
     -- TODO: validation
-    local old_version = global.mod_version[mod_name]
-    mod_reset_events(mod_name)
-    install_mod(mod_name, mod_version, mod_code)
-    run_mod(mod_name)
-    mod_load(mod_name)
+    local caller = game.player or _ENV
+    if (caller == _ENV) or caller.admin then
+        local old_version = global.mod_version[mod_name]
+        mod_reset_events(mod_name)
+        install_mod(mod_name, mod_version, mod_code)
+        run_mod(mod_name)
+        mod_load(mod_name)
 
-    -- The mod must do any migrations here
-    -- TODO: notify all mods
-    mod_configuration_changed(mod_name, {mod_changes = {mod_name={old_version=old_version, new_version=mod_version}}})
+        -- The mod must do any migrations here
+        -- TODO: notify all mods
+        mod_configuration_changed(mod_name, {mod_changes = {mod_name={old_version=old_version, new_version=mod_version}}})
+    end
 end
 
 --TODO: most of this function
@@ -295,9 +345,12 @@ remote_interface['clean'] = function(mod_name)
     -- Removes ALL mod data and reinitializes
     -- if the mod isnt designed for this, it will fail
     -- because it doesn't remove things like surfaces, etc
-    mod_reset(mod_name)
-    run_mod(mod_name)
-    mod_init(mod_name)
+    local caller = game.player or _ENV
+    if (caller == _ENV) or caller.admin then
+        mod_reset(mod_name)
+        run_mod(mod_name)
+        mod_init(mod_name)
+    end
 end
 
 remote.add_interface('hotpatch', remote_interface)
